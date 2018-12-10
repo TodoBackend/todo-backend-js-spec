0a704c51e409046c8da8378e310873f1ecd00dec
Patrik Turi <patrik.turi.0xff@gmail.com>
2018-12-11 00:26:30 +0100

    Install node modules

--- js/cli-test.js
+++ js/cli-test.js
@@ -1,3 +1,12 @@
+_ = require("underscore");
+Q = require("q");
+chai = require("chai")
+expect = chai.expect;
+chaiAsPromised = require("chai-as-promised");
+request = require('request');
+
+chai.use(chaiAsPromised);
+
 var specs = require("./specs.js");
 
 var address = process.env.TARGET_ADDRESS;
