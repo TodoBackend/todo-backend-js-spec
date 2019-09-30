acce3ac3c2f9450e5f511cfb79884d5809ef1a50
Pete Hodgson <git@thepete.net>
2019-09-30 15:54:02 -0700

    record test results to our services backend

--- js/crowdsource.js
+++ js/crowdsource.js
@@ -1,4 +1,8 @@
 (function(){
+
+var SERVICES_BASE = "https://services.todobackend.com/";
+var TEST_RUN_CREATE_URL = new URL('/test-runs', SERVICES_BASE);
+
 mocha.setup('bdd');
 mocha.slow("5s");
 mocha.timeout("30s"); //so that tests don't fail with a false positive while waiting for e.g a heroku dyno to spin up
@@ -17,6 +21,12 @@ if( targetRootUrl ){
 function runAndRecordTests(){
   mocha.checkLeaks();
   var runner = mocha.run();
+
+  var testResultsUrl = null;
+  startRecordingTestRun().then( function(testRunResource){
+    testResultsUrl = new URL(testRunResource._links.results.href, SERVICES_BASE);
+  });
+
   track('Test Start',{targetRootUrl:targetRootUrl});
 
   runner.on('suite end', function(suite){
@@ -24,10 +34,27 @@ function runAndRecordTests(){
       var suitePayload = serializeSuite(suite);
 
       track('Test Suite End', suitePayload);
-      //console.log('Test Suite End', suitePayload);
+      if( testResultsUrl ){
+        recordTestResults(testResultsUrl,suitePayload);
+      }else{
+        console.log('no test run url available to record results');
+      }
     }
   });
 };
+
+function startRecordingTestRun(){
+  return $.post(TEST_RUN_CREATE_URL);
+}
+
+function recordTestResults(testResultsUrl,results){
+  return $.ajax({
+    url: testResultsUrl,
+    method: 'POST',
+    contentType: 'application/json',
+    data: JSON.stringify(results)
+  });
+}
 
 function serializeSuite(suite){
   var childSuites = _.map( suite.suites, serializeSuite );
